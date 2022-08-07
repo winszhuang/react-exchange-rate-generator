6ed15c8a27f516c2b9284a9f6e5507ae8c06a07b
wins <win4709hi@gmail.com>
2022-08-07 11:14:10 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import React, { useState, MouseEventHandler } from 'react'
+import './App.css'
+
+interface CurrencyData {
+  key: string,
+  name: string,
+  rateFromTWD: string
+}
+
+const currencyDataList: CurrencyData[] = [
+  {
+    key: 'jpy',
+    name: '日幣',
+    rateFromTWD: '4.44'
+  },
+  {
+    key: 'usd',
+    name: '美金',
+    rateFromTWD: '0.033'
+  },
+  {
+    key: 'aud',
+    name: '澳幣',
+    rateFromTWD: '0.047'
+  },
+  {
+    key: 'krw',
+    name: '韓幣',
+    rateFromTWD: '30.02'
+  },
+  {
+    key: 'idr',
+    name: '印尼幣',
+    rateFromTWD: '497.17'
+  }
+]
+
+function generateRandomId () {
+  return Math.random().toString(16).slice(2)
+}
+
+function getValueById (id: string) {
+  return (document.getElementById(id) as HTMLInputElement | null)?.value || ''
+}
+
+function resetValueById (id: string) {
+  const el = (document.getElementById(id) as HTMLInputElement | null)
+  if (el) {
+    el.value = ''
+  }
+}
+
+function App () {
+  const [ownPrice, setOwnPrice] = useState(5000)
+  const [price, setPrice] = useState<number>(0)
+  const [currencyList, setCurrencyList] = useState([...currencyDataList])
+  const [recordList, setRecordList] = useState<string[]>([])
+
+  const setCurrentPrice: MouseEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault()
+    const price = getValueById('price')
+    setPrice(Number(price))
+  }
+
+  const addCurrency: MouseEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault()
+    const currencyName = getValueById('currencyName')
+    const currencyRate = getValueById('currencyRate')
+    if (currencyName && currencyRate) {
+      setCurrencyList([
+        ...currencyList,
+        {
+          key: generateRandomId(),
+          name: currencyName,
+          rateFromTWD: currencyRate
+        }
+      ])
+      resetValueById('currencyName')
+      resetValueById('currencyRate')
+    }
+  }
+
+  function exchange (currencyItem: CurrencyData) {
+    const { name, rateFromTWD } = currencyItem
+    setRecordList([
+      ...recordList,
+      `您用${price}元台幣，兌換了${(price * Number(rateFromTWD)).toFixed(2)}${name}`
+    ])
+    setOwnPrice(ownPrice - price)
+  }
+
+  return (
+    <>
+      <h3>新增幣種</h3>
+      <form>
+        <input type="text" placeholder="幣種名稱" id="currencyName" required/>
+        <input type="number" placeholder="匯率" id="currencyRate" required step=".001"/>
+        <input type="submit" value="新增幣種" onClick={addCurrency}/>
+      </form>
+      <hr></hr>
+
+      <h3>您錢包還有 {ownPrice} 元</h3>
+
+      <form>
+        <div>
+          請輸入您要換的台幣
+        </div>
+        <input type="number" placeholder="台幣" id="price" required/>
+        <input type="submit" value="計算" onClick={setCurrentPrice}/>
+      </form>
+
+      <p>可以換算</p>
+      <ul>
+        {(
+          currencyList.map(item => {
+            return (
+              <li key={item.key}>
+                {item.name}：{(price * Number(item.rateFromTWD)).toFixed(2) || ''}
+                <input type="button" value="兌換" onClick={() => exchange(item)}/>
+              </li>
+            )
+          })
+        )}
+      </ul>
+
+      <h3>您的兌換記錄</h3>
+      <ul>
+        {(
+          recordList.map(item => {
+            return (
+              <li key={item}>{item}</li>
+            )
+          })
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default App
